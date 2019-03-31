a422f173b1a67217bacfc53cc759b5cee0b99f6b
alwaysbaguette <alwaysbaguette@gmail.com>
2019-03-31 20:48:15 +0900

    styled-components and theme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
+
+import GlobalStyles from '../Styles/GlobalStyles';
+import Theme from '../Styles/Theme';
 
 const App = () =>{
   return(
-    <>
-      hello
-    </>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
 export default App;
