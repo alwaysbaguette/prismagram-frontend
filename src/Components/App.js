9c2cdd8c9aec7bf18649b1093b42d0c8a08931a1
alwaysbaguette <alwaysbaguette@gmail.com>
2019-04-01 13:31:08 +0900

    login sign up css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {gql} from 'apollo-boost';
-import {ThemeProvider} from 'styled-components';
+import styled ,{ThemeProvider} from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import AppRouter from './Router';
 import { useQuery } from 'react-apollo-hooks';
+import Footer from './Footer';
 
 
 const QUERY = gql`
@@ -13,16 +14,22 @@ const QUERY = gql`
   }
 `;
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`
 const App = () =>{
 
   const {data:{isLoggedIn}} = useQuery(QUERY);
   
   return(
     <ThemeProvider theme={Theme}>
-    <>
+    <Wrapper>
       <GlobalStyles />
       <AppRouter isLoggedIn={isLoggedIn} />
-    </>
+      <Footer />
+    </Wrapper >
     </ThemeProvider>
   )
 }
